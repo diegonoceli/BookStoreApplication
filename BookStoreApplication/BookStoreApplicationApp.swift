507b53362b04718d5170532c503fede0dcb4bfe6
Diego Noceli <diegonoceli@gmail.com>
2023-08-28 09:03:53 -0300

    Initial Commit

---
+++ BookStoreApplication/BookStoreApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApplicationApp.swift
+//  BookStoreApplication
+//
+//  Created by Diego Noceli on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookStoreApplication/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BookStoreApplication
+//
+//  Created by Diego Noceli on 28/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
