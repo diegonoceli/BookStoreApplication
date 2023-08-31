110881d0f93bc0b4d292485bbe791432b10d5065
Diego Noceli <diegonoceli@gmail.com>
2023-08-31 13:44:14 -0300

    v1.0

--- BookStoreApplication/ContentView.swift
+++ BookStoreApplication/ContentView.swift
@@ -12,168 +12,188 @@ struct ContentView: View {
     
     @State private var books: [Book] = [] // Populate this with your data source
     @State private var showFavoritesOnly = false
+    @State private var currentPage = 1
+    @State private var isLoading = false
     
     var body: some View {
         NavigationView {
             VStack {
-                Button(action: toggleFavoritesFilter) {
-                    Text(showFavoritesOnly ? "Show All" : "Show Favorites")
-                        .font(.headline)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
-                .padding()
+                Toggle(isOn: $showFavoritesOnly) {
+                                    Text("Show Favorites")
+                                }
+                                .padding()
                 
                 List(filteredBooks, id: \.self) { book in
+                    if book == books.last {
+                        LoadMoreView().onAppear {
+                            loadMoreBooks()
+                        }
+                    }
                     NavigationLink(destination: BookDetailView(book: book)) {
                         BookRow(book: book)
                     }
                 }
-                .navigationTitle("Book Store")
             }
             .onAppear {
                 BookStore.shared.fetchBooks(query: "iOS", maxResults: 20, startIndex: 0) { result in
                     switch result {
                     case .success(let fetchedBooks):
-                        print("Fetched Books: \(fetchedBooks)")
                         books = fetchedBooks
                     case .failure(let error):
                         print("Error during fetch books: \(error)")
                     }
                 }
             }
-        }
-    }
-        
-        private var filteredBooks: [Book] {
-            if showFavoritesOnly {
-                return books.filter { $0.isFavorite }
-            } else {
-                return books
-            }
-        }
-        
-        private func toggleFavoritesFilter() {
-            showFavoritesOnly.toggle()
-        }
-        
-    }
-    
-    struct BookRow: View {
-        let book: Book
-        var body: some View {
-            HStack {
-                AsyncImage(url: book.thumbnailURL) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 80, height: 120)
-                        .cornerRadius(8)
-                } placeholder: {
-                    ProgressView()
-                }
-                .frame(width: 80, height: 120)
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(book.title)
-                        .font(.headline)
-                    
-                    Text(book.authors.joined(separator: ", "))
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                    
-                    Spacer()
-                }
-            }
-            .padding(.vertical, 8)
+            .navigationTitle("Book Store")
         }
     }
     
-    struct BookDetailView: View {
-        @ObservedObject private var viewModel: BookDetailViewModel
+    private var filteredBooks: [Book] {
+        if showFavoritesOnly {
+            return books.filter { $0.isFavorite }
+        } else {
+            return books
+        }
+    }
+    
+    private func loadMoreBooks() {
+        guard !isLoading else {
+            return
+        }
         
-        init(book: Book) {
-            self.viewModel = BookDetailViewModel(book: book)
+        isLoading = true
+        
+        // You can modify this function to load more books from your data source
+        BookStore.shared.fetchBooks(query: "iOS", maxResults: 20, startIndex: currentPage * 20) { result in
+            switch result {
+            case .success(let fetchedBooks):
+                books.append(contentsOf: fetchedBooks)
+                currentPage += 1
+                isLoading = false
+            case .failure(let error):
+                isLoading = false
+            }
+        }
+    }
+    
+    private func toggleFavoritesFilter() {
+        showFavoritesOnly.toggle()
+    }
+    
+}
+
+struct BookRow: View {
+    let book: Book
+    var body: some View {
+        HStack {
+            AsyncImage(url: book.thumbnailURL) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 120)
+                    .cornerRadius(8)
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 80, height: 120)
             
+            VStack(alignment: .leading, spacing: 8) {
+                Text(book.title)
+                    .font(.headline)
+                
+                Text(book.authors.joined(separator: ", "))
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                
+                Spacer()
+            }
         }
+        .padding(.vertical, 8)
+    }
+}
+
+struct BookDetailView: View {
+    @ObservedObject private var viewModel: BookDetailViewModel
+    
+    init(book: Book) {
+        self.viewModel = BookDetailViewModel(book: book)
         
-        var body: some View {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
-                    if let thumbnailURL = viewModel.book.thumbnailURL {
-                        AsyncImage(url: thumbnailURL) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: .infinity)
-                                .cornerRadius(8)
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(height: 200)
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                if let thumbnailURL = viewModel.book.thumbnailURL {
+                    AsyncImage(url: thumbnailURL) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: .infinity)
+                            .cornerRadius(8)
+                    } placeholder: {
+                        ProgressView()
                     }
-                    
-                    Text(viewModel.book.title)
-                        .font(.title)
-                    
-                    Text("By \(viewModel.book.authors.joined(separator: ", "))")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                    
-                    Text(viewModel.book.description)
-                        .font(.body)
-                        .multilineTextAlignment(.leading)
-                    
-                    if let buyLinkURL = viewModel.book.buyLinkURL {
-                        Button(action: {
-                            openURL(buyLinkURL)
-                        }) {
-                            Text("Buy Now")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(Color.blue)
-                                .cornerRadius(8)
-                        }
+                    .frame(height: 200)
+                }
+                
+                Text(viewModel.book.title)
+                    .font(.title)
+                
+                Text("By \(viewModel.book.authors.joined(separator: ", "))")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                
+                Text(viewModel.book.description)
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+                
+                if let buyLinkURL = viewModel.book.buyLinkURL {
+                    Button(action: {
+                        openURL(buyLinkURL)
+                    }) {
+                        Text("Buy Now")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(8)
                     }
-                    
-                    Spacer()
                 }
-                .padding()
+                
+                Spacer()
             }
-            .onAppear{
-                viewModel.isFavorite = UserDefaults.standard.bool(forKey: viewModel.book.title)
-            }
-            .onDisappear {
-                UserDefaults.standard.set(viewModel.isFavorite, forKey: viewModel.book.title)
-                NotificationCenter.default.post(name: Notification.Name("refreshList"), object: nil)
-            }
-            .navigationTitle("Book Details")
-            .navigationBarItems(
-                trailing: Button(action: toggleFavorite) {
-                    Image(systemName: viewModel.isFavorite ? "heart.fill" : "heart")
-                        .foregroundColor(viewModel.isFavorite ? Color.red : Color.primary)
-                }
-            )
+            .padding()
         }
-        
-        private func toggleFavorite() {
-            viewModel.isFavorite.toggle()
+        .onAppear{
+            viewModel.isFavorite = UserDefaults.standard.bool(forKey: viewModel.book.title)
+        }
+        .onDisappear {
             UserDefaults.standard.set(viewModel.isFavorite, forKey: viewModel.book.title)
         }
+        .navigationTitle("Book Details")
+        .navigationBarItems(
+            trailing: Button(action: toggleFavorite) {
+                Image(systemName: viewModel.isFavorite ? "heart.fill" : "heart")
+                    .foregroundColor(viewModel.isFavorite ? Color.red : Color.primary)
+            }
+        )
     }
     
-    private func openURL(_ url: URL) {
-        if UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url)
-        }
+    private func toggleFavorite() {
+        viewModel.isFavorite.toggle()
+        UserDefaults.standard.set(viewModel.isFavorite, forKey: viewModel.book.title)
     }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+}
+
+private func openURL(_ url: URL) {
+    if UIApplication.shared.canOpenURL(url) {
+        UIApplication.shared.open(url)
     }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BookStoreApplication/LoadMoreView.swift
@@ -0,0 +1,19 @@
+//
+//  LoadMoreView.swift
+//  BookStoreApplication
+//
+//  Created by Diego Noceli on 31/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct LoadMoreView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            ProgressView()
+            Spacer()
+        }
+    }
+}
